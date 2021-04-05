d67bf007308ec01f1c29e7afbf4abeb08089afac
firuz.shermatov <firuz.shermatov@go.alif.academy>
2021-04-05 11:53:59 +0500

    Modified types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,11 +1,20 @@
 package types
 
 type Money int64
-
+//Category is the category on which the payment was provided
 type Category string
+
+type Status string
+
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status ="INPROGRESS"
+)
 
 type Payment struct {
 	ID int
 	Amount Money
 	Category Category
+	Status Status
 }
